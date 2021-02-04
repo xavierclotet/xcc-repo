0f65eecda23c767c5074d9f55fa1feef214c2bed
Xavi Clotet <xavi.clotet@credimarket.com>
2021-02-04 17:31:08 +0100

    first commit

---
+++ apps/xcc-repo/src/app/app.module.ts
@@ -0,0 +1,12 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { UiModule } from '@xcc-repo/ui';
+@NgModule({
+  declarations: [AppComponent],
+  imports: [BrowserModule, UiModule],
+  providers: [],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ jest.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+  projects: ['<rootDir>/apps/xcc-repo', '<rootDir>/libs/ui'],
+};
